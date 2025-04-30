dbe80c5c650daed3b3498ee28cf36ff84180efdc
Wang Tiaoke <wngtk@outlook.com>
2025-04-30 13:37:20 +0800

    Add dup4

---
+++ ch1/dup4/main.go
@@ -0,0 +1,63 @@
+// Exercise 1.4
+
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 10.
+//!+
+
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	files := os.Args[1:]
+	if len(files) == 0 {
+		counts := make(map[string]int)
+		countLines(os.Stdin, counts)
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
+		}
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup4: %v\n", err)
+				continue
+			}
+			countLines2(f)
+			f.Close()
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
+
+func countLines2(f *os.File) {
+	input := bufio.NewScanner(f)
+	counts := make(map[string]int)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%d\t%s\n", f.Name(), n, line)
+		}
+	}
+}
+
+//!-
