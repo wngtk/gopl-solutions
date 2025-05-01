1c0ab3f4cd74d1b802aca098e7c8fe6391157077
Wang Tiaoke <wngtk@outlook.com>
2025-05-01 15:34:53 +0800

    done with exercise 2.1

---
+++ ch2/tempconv/conv.go
@@ -0,0 +1,30 @@
+// exercise 2.1
+
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 41.
+
+//!+
+
+package tempconv
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converts a Celsius temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// KToC converts a Kelvin temperature to Celsius.
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// FToK converts a Fahrenheit temperature to Kelvin.
+func FToK(f Fahrenheit) Kelvin { return CToK(FToC(f)) }
+
+// KToF converts a Kelvin temperature to Fahrenheit.
+func KToF(k Kelvin) Fahrenheit { return CToF(KToC(k)) }
+
+//!-
