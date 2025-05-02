76f230adbf3980d1923e644a9eb3b34a4f9781e1
Wang Tiaoke <wngtk@outlook.com>
2025-05-02 12:33:08 +0800

    done with exercise 3.11

--- ch3/comma/main.go
+++ ch3/comma/main.go
@@ -22,12 +22,14 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
-		fmt.Printf("  %s\n", comma(os.Args[i]))
-		fmt.Printf("  %s\n", comma2(os.Args[i]))
+		// fmt.Printf("  %s\n", comma(os.Args[i]))
+		// fmt.Printf("  %s\n", comma2(os.Args[i]))
+		fmt.Printf("  %s\n", comma3(os.Args[i]))
 	}
 }
 
@@ -49,7 +51,7 @@ func comma2(s string) string {
 
 	// Use a bytes.Buffer to build the string efficiently.
 	var buf bytes.Buffer
-	
+
 	start := n % 3
 	if start == 0 {
 		start = 3
@@ -58,11 +60,25 @@ func comma2(s string) string {
 	// Write the first part of the string (before the first comma).
 	fmt.Fprintf(&buf, "%s", s[:start])
 
-	for i := start; i + 3 <= n; i += 3 {
+	for i := start; i+3 <= n; i += 3 {
 		fmt.Fprintf(&buf, ",%s", s[i:i+3])
 	}
 
 	return buf.String()
+}
+
+func comma3(s string) string {
+	flag := ""
+	if s[0] == '-' || s[0] == '+' {
+		flag = s[:1]
+		s = s[1:]
+	}
+
+	dot := strings.Index(s, ".")
+	if dot == -1 {
+		return flag + comma2(s)
+	}
+	return flag + comma2(s[:dot]) + "." + comma2(s[dot+1:])
 }
 
 //!-
