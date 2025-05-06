87098ce19e06e02784a4e3c3c8a9f10c8d6808fc
Wang Tiaoke <wngtk@outlook.com>
2025-05-06 14:01:50 +0800

    done with exercise 4.8

---
+++ ch4/charcount/main.go
@@ -0,0 +1,71 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 97.
+//!+
+
+// Charcount computes counts of Unicode characters.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+
+	countUnicodeLetters := 0
+	countUnicodeNumbers := 0
+	countUnicodeSpaces := 0
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+		if unicode.IsLetter(r) {
+			countUnicodeLetters++
+		} else if unicode.IsNumber(r) {
+			countUnicodeNumbers++
+		} else if unicode.IsSpace(r) {
+			countUnicodeSpaces++
+		}
+
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+	fmt.Printf("\n%d Unicode letters\n", countUnicodeLetters)
+	fmt.Printf("%d Unicode numbers\n", countUnicodeNumbers)
+	fmt.Printf("%d Unicode spaces\n", countUnicodeSpaces)
+}
+
+//!-
