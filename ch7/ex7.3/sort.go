7ce902c9b696c3eddb311ba2b11e8868ac1033e8
Wang Tiaoke <wngtk@outlook.com>
2025-06-16 16:25:43 +0800

    done ex7.3

---
+++ ch7/ex7.3/sort.go
@@ -0,0 +1,69 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 101.
+
+// Package treesort provides insertion sort using an unbalanced binary tree.
+package main
+
+import "fmt"
+
+// !+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice.
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value: value}.
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+
+func (t *tree) String() string {
+	var values []int
+	values = appendValues(values[:0], t)
+	return fmt.Sprintf("%v", values)
+}
+
+//!-
+
+func main() {
+	var root *tree
+	var arr = []int{77, 58, 25, 8}
+
+	for _, v := range arr {
+		root = add(root, v)
+	}
+
+	fmt.Println(root)
+}
